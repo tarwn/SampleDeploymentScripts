7fb261ca52787cfaf845d6a9454522011553dad6
Eli Weinstock-Herman <tarwn@tiernok.com>
2014-10-26 09:22:23 -0400

    Single commit example - needs a 2nd commit to work

--- GitHistoryPost.ps1
+++ GitHistoryPost.ps1
@@ -2,6 +2,9 @@
 # These are scripts that can be run directly in TeamCity build steps and utilize
 # the Predefined Build Parameters it supports
 
+Import-Module "./modules/GitHistory.psm1"
+
 # 1 - Get commit message for commit that triggered the build
 
-Get-CommitsFromGitLog 
+# Get-CommitsFromGitLog -StartCommit "%build.vcs.number%^" -EndCommit "%build.vcs.number%"
+Get-CommitsFromGitLog -StartCommit "a87199b17e6ac5ca51f7c9cf011bab416ebf0378^" -EndCommit "a87199b17e6ac5ca51f7c9cf011bab416ebf0378"
