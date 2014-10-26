2ae6fbc4dc75066dfbe9a0a870124c574e0b5f1c
Eli Weinstock-Herman <tarwn@tiernok.com>
2014-10-26 09:29:27 -0400

    Completed 1st example

--- GitHistoryPost.ps1
+++ GitHistoryPost.ps1
@@ -2,9 +2,9 @@
 # These are scripts that can be run directly in TeamCity build steps and utilize
 # the Predefined Build Parameters it supports
 
-Import-Module "./modules/GitHistory.psm1"
+Import-Module "./modules/GitHistory.psm1" -Force
 
 # 1 - Get commit message for commit that triggered the build
 
 # Get-CommitsFromGitLog -StartCommit "%build.vcs.number%^" -EndCommit "%build.vcs.number%"
-Get-CommitsFromGitLog -StartCommit "a87199b17e6ac5ca51f7c9cf011bab416ebf0378^" -EndCommit "a87199b17e6ac5ca51f7c9cf011bab416ebf0378"
+Get-CommitsFromGitLog -StartCommit "7fb261c^" -EndCommit "7fb261c"
--- modules/GitHistory.psm1
+++ modules/GitHistory.psm1
@@ -4,6 +4,6 @@ function Get-CommitsFromGitLog([string] $StartCommit, [string] $EndCommit){
     $Cmd = "git log --pretty=format:""CommitHash=%H :: AuthorEmail=%ae :: AuthorDate=%ad :: Subject=%s"" $StartCommit...$EndCommit"
     Write-Host "Git Command: $Cmd" 
     $Result = Invoke-Expression $Cmd
-    $ParsedResult = $Result | %{ Convert-FromString($_ -replace " :: ", "`n")  }
+    $ParsedResult = $Result | %{ ConvertFrom-StringData($_ -replace " :: ", "`n")  }
     return $ParsedResult
 }
